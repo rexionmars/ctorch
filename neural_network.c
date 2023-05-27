f735cb69cc98df08a6ecaa979268d1890820b782
rexionmars <opensource.leonardi@gmail.com>
2023-05-27 14:21:14 -0300

    Implemented math operations from the framework

--- neural_network.c
+++ neural_network.c
@@ -7,16 +7,22 @@
 int main(void)
 {
     srand(time(0));
-    Math a = mathematical_allocation(3, 3);
-    math_fill(a, 1);
+    Math a = mathematical_allocation(1, 2);
+    math_rand(a, 5, 10);
 
-    Math b = mathematical_allocation(3, 3);
-    math_fill(b, 1);
+    float id_date[4] = {
+        1, 0,
+        0, 1
+    };
+
+    Math b = { .rows = 2, .colluns = 2, .es = id_date };
+
+    Math distance = mathematical_allocation(1, 2);
 
     math_print(a);
-    printf("---------------------\n");
-    math_sum(a, b);
-    math_print(a);
+    printf("---------------\n");
+    math_dot(distance, a, b);
+    math_print(distance);
 
     return 0;
 }
